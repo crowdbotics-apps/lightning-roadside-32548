8001d5fcd8197e989e25cb00763ddd3632a2c4d0
Crowdbotics <team@crowdbotics.com>
2021-12-22 23:07:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lightning_roadside_32548",
+  url: "https://lightning_roadside_32548.botics.co",
+  api: "https://lightning_roadside_32548.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
